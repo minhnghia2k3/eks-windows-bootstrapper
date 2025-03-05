c24bbab30774e93744f5778f3f050585a6a6e800
omorrfaruk <128168538+omorrfaruk@users.noreply.github.com>
2025-03-05 14:55:00 +0000

    checked changes from 1.31 - no code changes, readme updated (#10)

--- BootstrapperService.cs
+++ BootstrapperService.cs
@@ -83,10 +83,10 @@ public class BootstrapperService : BackgroundService
             return;
         }
 
-        clusterName = Regex.Match(userData, "-EKSClusterName '([^']+)'")?.Groups[1]?.Value ?? throw new ArgumentException("Cluster name was not found in userdata, exiting");
-        dnsClusterIP = Regex.Match(userData, "-DNSClusterIP '([^']+)'")?.Groups[1]?.Value ?? throw new ArgumentException("DnsClusterIP was not found in userdata, exiting");
-        kubeletExtraArgs = Regex.Match(userData, "-KubeletExtraArgs '([^']+)'")?.Groups[1]?.Value ?? string.Empty;
-        kubeProxyExtraArgs = Regex.Match(userData, "-KubeProxyExtraArgs '([^']+)'").Groups[1].Value ?? string.Empty;
+        clusterName = Regex.Match(userData, "-EKSClusterName \"([^\"]+)\"")?.Groups[1]?.Value ?? throw new ArgumentException("Cluster name was not found in userdata, exiting");
+        dnsClusterIP = Regex.Match(userData, "-DNSClusterIP \"([^\"]+)\"")?.Groups[1]?.Value ?? throw new ArgumentException("DnsClusterIP was not found in userdata, exiting");
+        kubeletExtraArgs = Regex.Match(userData, "-KubeletExtraArgs \"([^\"]+)\"")?.Groups[1]?.Value ?? string.Empty;
+        kubeProxyExtraArgs = Regex.Match(userData, "-KubeProxyExtraArgs \"([^\"]+)\"")?.Groups[1]?.Value ?? string.Empty;
 
         _logger.LogInformation($"Extracted parameters: ClusterName: {clusterName}, DnsClusterIP: {dnsClusterIP}, KubeletExtraArgs: {kubeletExtraArgs}, KubeProxyExtraArgs: {kubeProxyExtraArgs}");
 
